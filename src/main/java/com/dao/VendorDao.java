f8202706a110b33efe1cc37569051cefc9800950
andrew.reut <aw.reut@andersenlab.com>
2017-12-13 12:16:35 +0200

    delete trash

--- src/main/java/com/dao/VendorDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-import com.entities.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorDao extends JpaRepository<Vendor, Integer> {
-}
