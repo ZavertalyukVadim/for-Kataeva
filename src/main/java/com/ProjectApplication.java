74fea1816cbf570360c840127f1a19faff9b46dc
andrew.reut <aw.reut@andersenlab.com>
2017-12-13 12:25:54 +0200

    add annotations

--- src/main/java/com/ProjectApplication.java
+++ src/main/java/com/ProjectApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,7 +15,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan(basePackages = "com")
-//@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+@EnableSpringDataWebSupport
+@EnableJpaRepositories
 public class ProjectApplication {
 
     public static void main(String[] args) {
