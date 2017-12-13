556e97e85204ebbb421b4c489d40221d72b357cb
andrew.reut <aw.reut@andersenlab.com>
2017-12-13 13:47:31 +0200

    add swager and package war

--- src/main/java/com/ProjectApplication.java
+++ src/main/java/com/ProjectApplication.java
@@ -2,11 +2,17 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-//@EnableSwagger2
+@EnableSwagger2
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableJpaRepositories
@@ -17,12 +23,12 @@ public class ProjectApplication {
         SpringApplication.run(ProjectApplication.class, args);
     }
 
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
