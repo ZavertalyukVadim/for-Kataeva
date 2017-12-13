d42b6994f31a3d6402ad40ecf379d2a5b7a2149d
andrew.reut <aw.reut@andersenlab.com>
2017-12-13 12:43:42 +0200

    small refactoring

--- src/main/java/com/config/WebMvcConfig.java
+++ src/main/java/com/config/WebMvcConfig.java
@@ -3,14 +3,12 @@ package com.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
