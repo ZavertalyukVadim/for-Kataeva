e716aac6780bb046576f793298961dddccf52777
andrew.reut <aw.reut@andersenlab.com>
2017-12-13 13:42:11 +0200

    add annotation for component scan

--- src/main/java/com/ProjectApplication.java
+++ src/main/java/com/ProjectApplication.java
@@ -3,6 +3,7 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.builders.PathSelectors;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableJpaRepositories
+@ComponentScan
 public class ProjectApplication {
 
     public static void main(String[] args) {
