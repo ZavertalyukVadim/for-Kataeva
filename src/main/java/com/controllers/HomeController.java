14b1aa292934568357e4e647414817ffc752ba37
andrew.reut <aw.reut@andersenlab.com>
2017-12-13 12:33:53 +0200

    add mapping

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -4,8 +4,10 @@ import com.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/")
 public class HomeController {
 
     private final ProjectService projectService;
